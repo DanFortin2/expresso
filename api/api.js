8074cefe612d60d95e89873802c48afed3628292
unknown <fortin_85@hotmail.com>
2018-12-23 11:09:29 -0500

    added starting base to api.js

--- api/api.js
+++ api/api.js
@@ -1,29 +1,9 @@
 const express = require('express');
-const app = express();
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const errorhandler = require('errorhandler')
-const apiRouter = require('./api/api');
+const apiRouter = express.Router();
+//const artRouter = require('./artists.js');
+//const seriesRouter = require('./series.js');
 
-//set port to listen on 4000
-const PORT = process.env.PORT || 4000;
+//apiRouter.use('/artists', artRouter);
+//apiRouter.use('/series', seriesRouter);
 
-//using middleware
-app.use(cors());
-app.use(bodyParser.json());
-app.use(morgan('dev'));
-
-app.use(express.static('public'));
-
-//using apiRouter from api.js file
-app.use('/api', apiRouter);
-
-app.use(errorhandler());
-
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
-
-
-module.exports = app;
+module.exports = apiRouter;
