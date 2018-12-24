f73e293a5f5307f80d1bc5cdb42471caca26f49f
unknown <fortin_85@hotmail.com>
2018-12-24 12:26:23 -0500

    post method working and creating new menu-items

--- api/menuitem.js
+++ api/menuitem.js
@@ -36,6 +36,52 @@ menuItemRouter.get('/', (req, res, next) => {
   });
 });
 
+//Helper function to validate the values in the req
+const validateMenuItems = (req, res, next) => {
+  const newMenuItems = req.body.menuItem;
+  const menuId = req.params.menuId;
+  if (!newMenuItems.name || !newMenuItems.description || !newMenuItems.inventory ||
+    !newMenuItems.price || !menuId ) {
+    return res.status(400).send();
+  }
+  next();
+}
+
+
+//create new Menu Items
+menuItemRouter.post('/', validateMenuItems, (req, res, next) => {
+  const newMenuItems = req.body.menuItem;
+  const menuId = req.params.menuId;
+  db.run(`INSERT INTO MenuItem (menu_id, name, description, inventory, price)
+  VALUES ($menuId, $name, $description, $inventory, $price)`,
+  {
+    $menuId : menuId,
+    $name : newMenuItems.name,
+    $description : newMenuItems.description,
+    $inventory : newMenuItems.inventory,
+    $price :  newMenuItems.price
+  },
+  function(err) {
+    if (err) {
+      next(err);
+    }
+    db.get(`SELECT * FROM MenuItem WHERE id = ${this.lastID}`, (err, menuItem) => {
+      if(!menuItem) {
+        res.status(500).send()
+      }
+      res.status(201).json( {menuItem : menuItem} );
+    });
+  });
+});
+
+
+
+//{ menuItem:
+  // { name: 'New Menu Item',
+    // description: 'New Description',
+    // inventory: 20,
+    // price: 1.5 } }
+
 
 
 
